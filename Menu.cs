11b5b18c22d431f6c11d9aa5c0f038b1919ca313
Carlos Gahel Pulido Cervantes <carlosgahelpulidocervantes@gmail.com>
2024-02-28 19:42:57 -0700

    Diseño de layout inicial

--- Menu.cs
+++ Menu.cs
@@ -16,5 +16,15 @@ namespace Proyecto_Regex
         {
             InitializeComponent();
         }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
